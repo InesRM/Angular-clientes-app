e4cdecd401ccf023da863486a61318cbf14a0a1e
InesRM <107609883+InesRM@users.noreply.github.com>
2023-01-11 22:11:45 +0100

    componentes listadoclientes, altaclientes y pintamos html, import y export de los componentes

--- src/app/clientes/clientes.module.ts
+++ src/app/clientes/clientes.module.ts
@@ -3,14 +3,21 @@ import { CommonModule } from '@angular/common';
 import { ClientesService } from './clientes.service';
 import { AltaClienteComponent } from './alta-cliente/alta-cliente.component';
 import { FormsModule } from '@angular/forms';
+import { ListadoClientesComponent } from './listado-clientes/listado-clientes.component';
 
 
 @NgModule({
   declarations: [
-    AltaClienteComponent
+    AltaClienteComponent,
+    ListadoClientesComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
+  ],
+  exports: [
+    AltaClienteComponent,
+    ListadoClientesComponent
   ],
   // Añadimos el servicio y primero lo hemos importado, se usa el arry providers
   providers: [ClientesService]
